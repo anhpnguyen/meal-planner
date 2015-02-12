676a8af6a52e811d2b7b87627b039b0d1c0f14d4
ANH NGUYEN <nguye376@msu.edu>
2015-02-12 14:13:45 -0500

    added Monday and Tuesday meals

---
+++ assets/js/content.js
@@ -0,0 +1,12 @@
+$(function(){
+	load_monday();
+	load_tuesday();
+});
+
+function load_monday(){
+	$("#monday").load("./monday.html", function())
+}
+function load_tuesday(){
+	$("#tuesday").load("./tuesday.html", function())
+}
+
